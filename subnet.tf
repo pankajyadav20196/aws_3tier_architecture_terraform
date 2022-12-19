d0bb93435a3f01ef4fa4cd818e9e5cf81e8a695c
pankajyadav20196 <101635654+pankajyadav20196@users.noreply.github.com>
2022-12-19 19:10:49 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
-  availability_zone = "ap-south-1b"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "private-sub3"
